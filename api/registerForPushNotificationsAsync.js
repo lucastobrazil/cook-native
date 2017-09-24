a77bf6d2ecbfbc5c07d16b37aa04672eacf4bb48
Lucas Arundell <larundell@small-improvements.com>
2017-09-24 15:56:38 +0200

    refactor(prettier) run format

--- api/registerForPushNotificationsAsync.js
+++ api/registerForPushNotificationsAsync.js
@@ -5,29 +5,29 @@ import { Permissions, Notifications } from 'expo';
 const PUSH_ENDPOINT = 'http://5acf2105.ngrok.io';
 
 export default (async function registerForPushNotificationsAsync() {
-  // Android remote notification permissions are granted during the app
-  // install, so this will only ask on iOS
-  let { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    let { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
 
-  // Stop here if the user did not grant permissions
-  if (status !== 'granted') {
-    return;
-  }
+    // Stop here if the user did not grant permissions
+    if (status !== 'granted') {
+        return;
+    }
 
-  // Get the token that uniquely identifies this device
-  let token = await Notifications.getExpoPushTokenAsync();
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
 
-  // POST the token to our backend so we can use it to send pushes from there
-  return fetch(PUSH_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      token: {
-        value: token,
-      },
-    }),
-  });
+    // POST the token to our backend so we can use it to send pushes from there
+    return fetch(PUSH_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token: {
+                value: token,
+            },
+        }),
+    });
 });
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -10,51 +10,48 @@ import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 export default TabNavigator(
-  {
-    Home: {
-      screen: HomeScreen,
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        Links: {
+            screen: LinksScreen,
+        },
+        Settings: {
+            screen: SettingsScreen,
+        },
     },
-    Links: {
-      screen: LinksScreen,
-    },
-    Settings: {
-      screen: SettingsScreen,
-    },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        switch (routeName) {
-          case 'Home':
-            iconName = Platform.OS === 'ios'
-              ? `ios-information-circle${focused ? '' : '-outline'}`
-              : 'md-information-circle';
-            break;
-          case 'Links':
-            iconName = Platform.OS === 'ios'
-              ? `ios-link${focused ? '' : '-outline'}`
-              : 'md-link';
-            break;
-          case 'Settings':
-            iconName = Platform.OS === 'ios'
-              ? `ios-options${focused ? '' : '-outline'}`
-              : 'md-options';
-        }
-        return (
-          <Ionicons
-            name={iconName}
-            size={28}
-            style={{ marginBottom: -3 }}
-            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
-      },
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false,
-  }
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                switch (routeName) {
+                    case 'Home':
+                        iconName =
+                            Platform.OS === 'ios'
+                                ? `ios-information-circle${focused ? '' : '-outline'}`
+                                : 'md-information-circle';
+                        break;
+                    case 'Links':
+                        iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
+                        break;
+                    case 'Settings':
+                        iconName = Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+                }
+                return (
+                    <Ionicons
+                        name={iconName}
+                        size={28}
+                        style={{ marginBottom: -3 }}
+                        color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+                    />
+                );
+            },
+        }),
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: 'bottom',
+        animationEnabled: false,
+        swipeEnabled: false,
+    }
 );
